8a071eaf3d16cf01fa77d010bba9f77621d96830
Chris Olsen <olsen.chris@gmail.com>
2017-02-03 15:26:35 -0700

    remove no longer needed interface method

--- model/model.go
+++ model/model.go
@@ -6,8 +6,3 @@ import "google.golang.org/appengine/datastore"
 type Base struct {
 	Key *datastore.Key `json:"key" datastore:"-"`
 }
-
-// SetKey allows the key of a model to be auto set
-func (b *Base) SetKey(k *datastore.Key) {
-	b.Key = k
-}
