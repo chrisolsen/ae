2fb4d8cf6b9d6d3728d2546d727d63334000b04b
Chris Olsen <olsen.chris@gmail.com>
2017-02-04 22:00:34 -0700

    remove duplicate error handling

--- auth/middleware.go
+++ auth/middleware.go
@@ -9,7 +9,6 @@ import (
 	"golang.org/x/net/context"
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/datastore"
-	"google.golang.org/appengine/log"
 	"google.golang.org/appengine/memcache"
 )
 
@@ -37,63 +36,53 @@ type Middleware struct {
 
 // AuthenticateCookie authenticates the token with a request cookie
 func (m *Middleware) AuthenticateCookie(c context.Context, w http.ResponseWriter, r *http.Request) context.Context {
-	var err error
-	c, cancel := context.WithCancel(c)
+	var accountKey *datastore.Key
 
+	c, cancel := context.WithCancel(c)
 	returnURL := fmt.Sprintf("/signin?returnUrl=%s", r.RequestURI)
 
-	cookie, err := r.Cookie(cookieName)
-	if err != nil {
-		if !m.ContinueWithBadToken {
-			http.Redirect(w, r, returnURL, http.StatusTemporaryRedirect)
-			cancel()
-		}
-		return c
-	}
-
-	token, err := m.getToken(c, cookie.Value)
-	if err != nil {
-		if !m.ContinueWithBadToken {
-			http.Redirect(w, r, returnURL, http.StatusTemporaryRedirect)
-			cancel()
-		}
-		return c
-	}
-
-	// if token has expired return 401
-	if token.isExpired() {
-		if !m.ContinueWithBadToken {
-			http.Redirect(w, r, returnURL, http.StatusTemporaryRedirect)
-			cancel()
-		}
-		return c
-	}
-
-	accountKey := token.Key.Parent()
-
-	// if the token's expiry less than a week away, get new token
-	if token.willExpireIn(time.Hour * 24 * 7) {
-		newToken, err := m.getNewToken(c, accountKey)
+	run := func() error {
+		cookie, err := r.Cookie(cookieName)
 		if err != nil {
-			if !m.ContinueWithBadToken {
-				http.Redirect(w, r, returnURL, http.StatusTemporaryRedirect)
-				cancel()
-			}
-			return c
+			return fmt.Errorf("failed getting cookie: %v", err)
 		}
 
-		// send back the new token values
-		http.SetCookie(w, &http.Cookie{
-			Name:     cookieName,
-			Expires:  time.Now().Add(time.Hour * 24 * 14), // 2 weeks from now
-			HttpOnly: true,
-			Secure:   !appengine.IsDevAppServer(),
-			Value:    newToken.Value(),
-		})
+		token, err := m.getToken(c, cookie.Value)
+		if err != nil {
+			return fmt.Errorf("failed to get token: %v", err)
+		}
+		if token.isExpired() {
+			return errors.New("expired token")
+		}
+
+		accountKey = token.Key.Parent()
+
+		// if the token's expiry less than a week away, get new token and kill the current
+		if token.willExpireIn(time.Hour * 24 * 7) {
+			newToken, err := m.getNewToken(c, token)
+			if err != nil {
+				return fmt.Errorf("failed to get new token: %v", err)
+			}
+
+			http.SetCookie(w, &http.Cookie{
+				Name:     cookieName,
+				Expires:  time.Now().Add(time.Hour * 24 * 14),
+				HttpOnly: true,
+				Secure:   !appengine.IsDevAppServer(),
+				Value:    newToken.Value(),
+			})
+		}
+
+		// add accountKey to context
+		c = m.Session.SetAccountKey(c, accountKey)
+
+		return nil
 	}
 
-	// add accountKey to context
-	c = m.Session.SetAccountKey(c, accountKey)
+	if err := run(); err != nil && !m.ContinueWithBadToken {
+		http.Redirect(w, r, returnURL, http.StatusTemporaryRedirect)
+		cancel()
+	}
 
 	return c
 }
@@ -105,71 +94,54 @@ func (m *Middleware) AuthenticateToken(c context.Context, w http.ResponseWriter,
 		return c
 	}
 
-	var err error
 	c, cancel := context.WithCancel(c)
 
-	authHeader := r.Header.Get("Authorization")
-	if len(authHeader) <= len("token=") {
-		log.Errorf(c, "missing token header: %v", err)
-		if !m.ContinueWithBadToken {
-			w.WriteHeader(http.StatusUnauthorized)
-			cancel()
+	run := func() error {
+		authHeader := r.Header.Get("Authorization")
+		if len(authHeader) <= len("token=") {
+			return errors.New("missing token header")
 		}
-		return c
-	}
 
-	// prevent token caching with blank string value
-	rawToken := authHeader[len("token="):]
-	if len(rawToken) == 0 {
-		log.Errorf(c, "missing token value: %v", err)
-		if !m.ContinueWithBadToken {
-			w.WriteHeader(http.StatusUnauthorized)
-			cancel()
+		// prevent token caching with blank string value
+		rawToken := authHeader[len("token="):]
+		if len(rawToken) == 0 {
+			return errors.New("missing token value")
 		}
-		return c
-	}
 
-	token, err := m.getToken(c, rawToken)
-	if err != nil {
-		log.Errorf(c, "failed to get token details: %v", err)
-		if !m.ContinueWithBadToken {
-			w.WriteHeader(http.StatusUnauthorized)
-			cancel()
-		}
-		return c
-	}
-
-	// if token has expired return 401
-	if token.isExpired() {
-		log.Errorf(c, "expired Token")
-		if !m.ContinueWithBadToken {
-			w.WriteHeader(http.StatusUnauthorized)
-			cancel()
-		}
-		return c
-	}
-
-	accountKey := token.Key.Parent()
-
-	// if the token's expiry less than a week away, get new token
-	if token.willExpireIn(time.Hour * 24 * 7) {
-		newToken, err := m.getNewToken(c, accountKey)
+		token, err := m.getToken(c, rawToken)
 		if err != nil {
-			log.Errorf(c, "failed to create new token: %v", err)
-			if !m.ContinueWithBadToken {
-				w.WriteHeader(http.StatusUnauthorized)
-				cancel()
-			}
-			return c
+			return fmt.Errorf("failed to get token details: %v", err)
 		}
 
-		// send back the new token values
-		w.Header().Add(newTokenHeader, newToken.Value())
-		w.Header().Add(newTokenExpiryHeader, newToken.Expiry.Format(time.RFC3339))
+		// if token has expired return 401
+		if token.isExpired() {
+			return errors.New("expired token")
+		}
+
+		accountKey := token.Key.Parent()
+
+		// if the token's expiry less than a week away, get new token
+		if token.willExpireIn(time.Hour * 24 * 7) {
+			newToken, err := m.getNewToken(c, token)
+			if err != nil {
+				return fmt.Errorf("failed to create new token: %v", err)
+			}
+
+			// send back the new token values
+			w.Header().Add(newTokenHeader, newToken.Value())
+			w.Header().Add(newTokenExpiryHeader, newToken.Expiry.Format(time.RFC3339))
+		}
+
+		// add accountKey to context
+		c = m.Session.SetAccountKey(c, accountKey)
+
+		return nil
 	}
 
-	// add accountKey to context
-	c = m.Session.SetAccountKey(c, accountKey)
+	if err := run(); err != nil && !m.ContinueWithBadToken {
+		w.WriteHeader(http.StatusUnauthorized)
+		cancel()
+	}
 
 	return c
 }
