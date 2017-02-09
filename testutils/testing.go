5900a4b678e54e017721632f48659ee0ccfe4937
Chris Olsen <olsen.chris@gmail.com>
2017-02-09 12:57:29 -0700

    add testutils docs

--- testutils/testing.go
+++ testutils/testing.go
@@ -6,10 +6,32 @@ import (
 	"google.golang.org/appengine/aetest"
 )
 
+// Simplifies creating contexts for tests and allows tests to use the same
+// context which will greatly speed up tests (by like a factor of 1000)
+// Example Setup;
+//  var T = testutils.T{}
+//
+//  func TestMain(m *testing.M) {
+//  	os.Exit(func() int {
+//  		code := m.Run()
+//  		T.Close()
+//  		return code
+//  	}())
+//  }
+//
+//  func TestSomething(t testing.T) {
+//  	c := T.GetContext()
+//  	k := ...
+//  	datastore.Get(c, key, nil)
+//  }
+
+// T contains a reference to a aetest.Instance to allow for faster tests
+// and closing of the test on completion
 type T struct {
 	inst aetest.Instance
 }
 
+// GetContext returns a new or cached context reference
 func (t *T) GetContext() context.Context {
 	inst := t.getInstance()
 	r, err := inst.NewRequest("GET", "/", nil)
@@ -28,6 +50,7 @@ func (t *T) getInstance() aetest.Instance {
 	return t.inst
 }
 
+// Close closes the testing instance
 func (t *T) Close() {
 	if t.inst != nil {
 		t.inst.Close()
