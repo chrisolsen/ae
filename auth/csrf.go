b0912fcf08b910b4c67c1c9e74b469a5abe440fe
Chris Olsen <olsen.chris@gmail.com>
2017-02-26 17:15:18 -0700

    ensure form is parsed correctly if it's multipart

--- auth/csrf.go
+++ auth/csrf.go
@@ -1,9 +1,12 @@
 package auth
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 
 	"golang.org/x/net/context"
+	"google.golang.org/appengine/log"
 )
 
 // NewCSRFToken creates token
@@ -29,22 +32,39 @@ func VerifyCSRFToken(c context.Context, w http.ResponseWriter, r *http.Request) 
 		return c
 	}
 
+	var csrf string
 	c2, cancel := context.WithCancel(c)
-	r.ParseForm()
-	csrf := r.FormValue("csrfToken")
 
-	// var auth Token
-	uuid, err := getUUIDFromCookie(r)
-	if err == ErrNoCookie {
-		uuid = anonUUID
-	} else if err != nil {
-		cancel()
-		return c2
-	}
+	err := func() error {
+		if strings.Contains(r.Header.Get("Content-Type"), "multipart/form-data") {
+			if err := r.ParseMultipartForm(10 << 20); err != nil {
+				return fmt.Errorf("error parsing multipart data: %v", err)
+			}
+			csrf = r.MultipartForm.Value["csrfToken"][0]
+		} else {
+			if err := r.ParseForm(); err != nil {
+				return fmt.Errorf("error pareing data: %v", err)
+			}
+			csrf = r.FormValue("csrfToken")
+		}
 
-	if err := checkCrypt(csrf, csrfSecret+uuid); err != nil {
+		// var auth Token
+		uuid, err := getUUIDFromCookie(r)
+		if err == ErrNoCookie {
+			uuid = anonUUID
+		} else if err != nil {
+			return err
+		}
+
+		if err := checkCrypt(csrf, csrfSecret+uuid); err != nil {
+			return fmt.Errorf("failed checkCrypt: %v", err)
+		}
+		return nil
+	}()
+
+	if err != nil {
+		log.Errorf(c, err.Error())
 		cancel()
-		return c2
 	}
 
 	return c2
