e99f5ceee08518ca872f19d86b088deba0d8ffd3
Chris Olsen <olsen.chris@gmail.com>
2017-03-10 18:34:06 -0700

    add middleware to add multiple response headers

--- que/headers/headers.go
+++ que/headers/headers.go
@@ -14,3 +14,13 @@ func Set(key, value string) que.Middleware {
 		return c
 	}
 }
+
+// SetMulti sets the response header to the keys/values provided
+func SetMulti(vals map[string]string) que.Middleware {
+	return func(c context.Context, w http.ResponseWriter, r *http.Request) context.Context {
+		for key, val := range vals {
+			w.Header().Set(key, val)
+		}
+		return c
+	}
+}
