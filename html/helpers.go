97198cad262a3ece920dfc843ecacc1c5e82b7fe
Chris Olsen <olsen.chris@gmail.com>
2017-03-25 15:12:31 -0600

    add some common html template functions

--- html/helpers.go
+++ html/helpers.go
@@ -2,8 +2,11 @@ package html
 
 import (
 	"html/template"
+	"time"
 
+	"github.com/chrisolsen/ae/model"
 	"github.com/russross/blackfriday"
+	"google.golang.org/appengine/datastore"
 )
 
 // CSS prevents any custom embedded styles from being encoded to html safe values
@@ -28,4 +31,54 @@ func Markdown(input string) interface{} {
 // Add adds the numbers
 func Add(a, b int) int {
 	return a + b
+}
+
+// EncodeKey encodes a datastore key
+func EncodeKey(data interface{}) string {
+	switch data.(type) {
+	case model.Base:
+		return data.(model.Base).Key.Encode()
+	case *datastore.Key:
+		return data.(*datastore.Key).Encode()
+	default:
+		return ""
+	}
+}
+
+// Checked returns the checked attribute for positive values.
+// 	<input type="checkbox" {{IsAdmin | checked}}> => <input type="checkbox" checked="checked">
+func Checked(checked bool) template.HTMLAttr {
+	if checked {
+		return template.HTMLAttr(`checked="checked"`)
+	}
+	return ""
+}
+
+// Disabled returns the checked attribute for positive values.
+// 	<button type="submit" {{HasError | disabled}}>Save</button> => <button type="submit" disabled="">Save</button>
+// 	or
+// 	<button type="submit" {{ValidationError | disabled}}>Save</button> => <button type="submit" disabled="">Save</button>
+func Disabled(err interface{}) template.HTMLAttr {
+	d := template.HTMLAttr(`disabled="disabled"`)
+	switch err.(type) {
+	case string:
+		if len(err.(string)) == 0 {
+			return ""
+		}
+		return d
+	case error:
+		return d
+	default:
+		return ""
+	}
+}
+
+// KeyEqual allow for *datastore.Key comparison
+func KeyEqual(a, b *datastore.Key) bool {
+	return a.Equal(b)
+}
+
+// Timestamp formats the time to the RFC3339 layout
+func Timestamp(d time.Time) string {
+	return d.Format(time.RFC3339)
 }
