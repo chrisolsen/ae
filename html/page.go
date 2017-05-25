6b9bcb1c226fabd8d931a290bb17261440d594f0
Chris Olsen <olsen.chris@gmail.com>
2017-05-24 22:53:56 -0600

    remove page offsets helper method

--- html/page.go
+++ html/page.go
@@ -2,7 +2,6 @@ package html
 
 import (
 	"errors"
-	"math"
 	"net/http"
 
 	"github.com/chrisolsen/ae/auth"
@@ -44,24 +43,4 @@ func (p Page) SetUser(user interface{}, _ ...interface{}) {
 // Set sets the key and value
 func (p Page) Set(key string, val interface{}) {
 	p[key] = val
-}
-
-// SetPageOffsets sets an array within the page to allow it to be iterated through
-// in the template to create pagination links.
-//  // .go file
-// 	p := html.NewPage()
-// 	items := []string {"foo", "bar", "bits", ...}
-// 	p.SetPageOffsets(len(items), 10)
-//
-//  // template
-//  {{range $index, $offset := .Offsets}}
-//      <a href="/name?o={{$offset}}">{{add $index 1}}</a>
-//  {{end}}
-func (p Page) SetPageOffsets(itemCount, pageSize int) {
-	offsetCount := int(math.Ceil(float64(itemCount) / float64(pageSize)))
-	offsets := make([]int, offsetCount)
-	for i := 0; i < offsetCount; i++ {
-		offsets[i] = i * 10
-	}
-	p.Set("Offsets", offsets)
 }
